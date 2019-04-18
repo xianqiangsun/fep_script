2342882137315d3abcc696626221e36b58e12f10
leos <leos@localhost.localdomain>
2019-04-18 09:41:26 -0400

    update config to consistent

--- sire/production/scripts/serial.sh
+++ sire/production/scripts/serial.sh
@@ -6,26 +6,6 @@
 export OPENMM_PLUGIN_DIR=$SIREHOME/lib/plugins
 cd output
 
-# do the equilibration at lamda=0.5
-#mid=0.5
-#mkdir lambda-$mid
-#cd lambda-$mid
-#somd-freenrg -C ../../input/sim_min.cfg -l $mid -p CUDA
-#somd-freenrg -C ../../input/sim_nvt_1.cfg -l $mid -p CUDA
-#somd-freenrg -C ../../input/sim_nvt_2.cfg -l $mid -p CUDA
-#somd-freenrg -C ../../input/sim_nvt_3.cfg -l $mid -p CUDA
-#somd-freenrg -C ../../input/sim_nvt_4.cfg -l $mid -p CUDA
-#somd-freenrg -C ../../input/sim_nvt_5.cfg -l $mid -p CUDA
-#somd-freenrg -C ../../input/sim_npt_1.cfg -l $mid -p CUDA
-#somd-freenrg -C ../../input/sim_npt_2.cfg -l $mid -p CUDA
-#rm -rf traj*
-#rm -rf SYSTEM*
-#rm -rf *dat
-#rm -rf gradients*
-#cp sim_restart.s3 ../sim_restart.s3.npt
-#cd ..
-#rm -rf lambda-$mid
-
 lamvals=( 0.0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0 )
 
 
@@ -37,11 +17,7 @@ for lam in "${lamvals[@]}"
     somd-freenrg -C ../../input/sim_min.cfg -l $lam -p CUDA
     somd-freenrg -C ../../input/sim_nvt_1.cfg -l $lam -p CUDA
     somd-freenrg -C ../../input/sim_npt_2.cfg -l $lam -p CUDA
-    rm -rf traj*
-    rm -rf *dat
-    rm -rf gradients*
     somd-freenrg -C ../../input/sim_md.cfg -l $lam -p CUDA
     cd ..
     done
-rm -rf sim_restart.s3.npt
 
