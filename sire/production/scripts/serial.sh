205ace9668999a185098b575e8d74f03c938b100
leos <leos@localhost.localdomain>
2019-04-19 10:54:23 -0400

    update to have the short simulation in case of stop

--- sire/production/scripts/serial.sh
+++ sire/production/scripts/serial.sh
@@ -13,17 +13,28 @@ cd output
 
 lamvals=( 0.0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0 )
 
-
 for lam in "${lamvals[@]}"
-    do
-    echo "lambda is: " $lam
-    mkdir lambda-$lam
-    cd lambda-$lam
-    cp ../sim_restart.s3.npt sim_restart.s3
-    somd-freenrg -C ../../input/sim_min.cfg -l $lam -p CUDA
-    somd-freenrg -C ../../input/sim_nvt_1.cfg -l $lam -p CUDA
-    somd-freenrg -C ../../input/sim_npt_2.cfg -l $lam -p CUDA
-    somd-freenrg -C ../../input/sim_md.cfg -l $lam -p CUDA
-    cd ..
-    done
+do
+echo "lambda is: " $lam
+mkdir lambda-$lam
+cd lambda-$lam
+somd-freenrg -C ../../input/sim_min.cfg -l $lam -p CUDA
+#rm -rf *.dat
+somd-freenrg -C ../../input/sim_nvt_1.cfg -l $lam -p CUDA
+#rm -rf *.dat
+somd-freenrg -C ../../input/sim_nvt_2.cfg -l $lam -p CUDA
+#rm -rf *.dat
+somd-freenrg -C ../../input/sim_nvt_3.cfg -l $lam -p CUDA
+#rm -rf *.dat
+somd-freenrg -C ../../input/sim_nvt_4.cfg -l $lam -p CUDA
+#rm -rf *.dat
+somd-freenrg -C ../../input/sim_nvt_5.cfg -l $lam -p CUDA
+#rm -rf *.dat
+somd-freenrg -C ../../input/sim_npt_1.cfg -l $lam -p CUDA
+#rm -rf *.dat
+somd-freenrg -C ../../input/sim_npt_2.cfg -l $lam -p CUDA
+#rm -rf *dat
+somd-freenrg -C ../../input/sim_md.cfg -l $lam -p CUDA
+cd ..
+done
 
